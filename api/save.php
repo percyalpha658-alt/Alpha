b106ee1e7cb73127a15c5c505e9b75f124b8b9e1
percyalpha658-alt <percyalpha658@gmail.com>
2026-07-15 19:04:40 +0200

    Implement WhatsApp alert for low soil humidity
    Added WhatsApp notification for low soil moisture.

--- api/save.php
+++ api/save.php
@@ -1,16 +1,43 @@
 <?php
 
-// Récupération des données envoyées par ESP32
 $temp = $_GET['temp'] ?? 0;
-$hum  = $_GET['hum'] ?? 0;
-$sol  = $_GET['sol'] ?? 0;
+$hum = $_GET['hum'] ?? 0;
+$sol = $_GET['sol'] ?? 0;
 $pompe = $_GET['pompe'] ?? 0;
 
-// Affichage de test
+
+// Configuration WhatsApp
+$phone = "243833374742";
+$apikey = "3019872";
+
+
+// Fonction envoi WhatsApp
+function envoyerWhatsApp($message, $phone, $apikey)
+{
+    $url = "https://api.callmebot.com/whatsapp.php?phone="
+    .$phone."&text=".urlencode($message)
+    ."&apikey=".$apikey;
+
+    file_get_contents($url);
+}
+
+
+// Alerte si sol sec
+if($sol < 60)
+{
+    $message = "⚠️ Smart Agriculture\n";
+    $message .= "Humidite du sol faible : ".$sol."%\n";
+    $message .= "Pompe : ".$pompe;
+
+    envoyerWhatsApp($message, $phone, $apikey);
+}
+
+
+// Réponse serveur
 echo "ESP32 connecté\n";
 echo "Temperature : ".$temp." °C\n";
 echo "Humidite air : ".$hum." %\n";
 echo "Humidite sol : ".$sol." %\n";
-echo "Pompe : ".$pompe."\n";
+echo "Pompe : ".$pompe;
 
 ?>
